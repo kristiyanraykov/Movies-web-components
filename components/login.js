868e419da90a429febc52ca79219ae3b3a562219
Kristiyan Raykov <craykov@yahoo.com>
2021-03-08 01:19:07 +0200

    added successful login

--- components/login.js
+++ components/login.js
@@ -1,8 +1,8 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
-import { register } from '../services/authServices.js';
+import { login } from '../services/authServices.js';
 
 const template = (ctx) => html`
-<form class="text-center border border-light p-5" action="#" method="post" @submit=${ctx.onSubmit}>
+<form class="text-center border border-light p-5" action="" method="" @submit=${ctx.onSubmit}>
 <div class="form-group">
     <label for="email">Email</label>
     <input type="email" class="form-control" placeholder="Email" name="email" value="">
@@ -12,15 +12,10 @@ const template = (ctx) => html`
     <input type="password" class="form-control" placeholder="Password" name="password" value="">
 </div>
 
-<div class="form-group">
-    <label for="repeatPassword">Repeat Password</label>
-    <input type="password" class="form-control" placeholder="Repeat-Password" name="repeatPassword" value="">
-</div>
-
-<button type="submit" class="btn btn-primary">Register</button>
+<button type="submit" class="btn btn-primary">Login</button>
 </form>`
 
-class Register extends HTMLElement {
+class Login extends HTMLElement {
     connectedCallback() {
         this.render();
     }
@@ -29,18 +24,10 @@ class Register extends HTMLElement {
         let formData = new FormData (e.target);
         let email = formData.get('email');
         let password = formData.get('password');
-        let repeatPassword = formData.get('repeatPassword');
-        if (password.length < 6){
-            notify('pass too short', 'error')
-            return;
-        }
-        if (password !== repeatPassword){
-            notify('pass must match', 'error')
-            return;
-        }
-        register(email, password)
+
+        login(email, password)
         .then(res => {
-            notify('Successful Registration', 'success');
+            notify('Successful Login', 'success');
             //TO DO REDIRECT To HOME
         })
         .catch(err => {
@@ -52,4 +39,4 @@ class Register extends HTMLElement {
     }
 };
 
-export default Register;
+export default Login;
--- services/authServices.js
+++ services/authServices.js
@@ -1,10 +1,26 @@
 const apiKey = 'AIzaSyCLBDc1jMaFB7ykl23VgjAKLyOxGg3PWE8';
 const databaseUrl = 'https://my-movies-a2d2f-default-rtdb.firebaseio.com';
-const registerUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
+
+
+const api = {
+    register: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+    login: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
+}
 import {request} from './requestServices.js';
 
 export const register = async (email, password) => {
-    let res = await request(registerUrl, 'POST', {
+    let res = await request(api.register, 'POST', {
+        email,
+        password,
+    });
+
+    localStorage.setItem('auth', JSON.stringify(res));
+
+    return res;
+}
+
+export const login = async (email, password) => {
+    let res = await request(api.login, 'POST', {
         email,
         password,
     });
