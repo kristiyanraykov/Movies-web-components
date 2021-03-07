a8b6c8677b0deefea6f054356ceffd53d8079461
Kristiyan Raykov <craykov@yahoo.com>
2021-03-08 00:21:26 +0200

    added register onSubmit

--- components/register.js
+++ components/register.js
@@ -1,7 +1,47 @@
+import {html, render} from 'https://unpkg.com/lit-html?module';
+
+const template = (ctx) => html`
+<form class="text-center border border-light p-5" action="#" method="post" @submit=${ctx.onSubmit}>
+<div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" placeholder="Email" name="email" value="">
+</div>
+<div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" placeholder="Password" name="password" value="">
+</div>
+
+<div class="form-group">
+    <label for="repeatPassword">Repeat Password</label>
+    <input type="password" class="form-control" placeholder="Repeat-Password" name="repeatPassword" value="">
+</div>
+
+<button type="submit" class="btn btn-primary">Register</button>
+</form>`
+
 class Register extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = 'In register Component'
+        this.render();
     }
-}
+    onSubmit(e){
+        e.preventDefault();
+        let formData = new FormData (e.target);
+        let email = formData.get('email');
+        let password = formData.get('password');
+        let repeatPassword = formData.get('repeatPassword');
+        if (password.length < 6){
+            console.error('pass too short');
+            return;
+        }
+        if (password !== repeatPassword){
+            console.error('pass must match');
+            return;
+        }
+        console.log('success');
+    }
+    render(){
+        render(template(this), this, { eventContext: this} );
+    }
+};
 
 export default Register;
